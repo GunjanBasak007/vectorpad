25d0137fb9fbf07fa813ea6294c056f24072c13b
GunjanBasak007 <gunjanbasak07@gmail.com>
2026-03-27 21:24:12 +0530

    Deleted unneccessary logo from create team route

--- app/(routes)/teams/create/page.tsx
+++ app/(routes)/teams/create/page.tsx
@@ -4,7 +4,6 @@ import { Input } from '@/components/ui/input'
 import { api } from '@/convex/_generated/api'
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import { useMutation } from 'convex/react'
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import { toast } from 'sonner'
@@ -30,10 +29,7 @@ function CreateTeam() {
   }
   return (
     <div className=' px-6 md:px-16 my-16'>
-      <Image src='/logo-black.png'
-      alt='logo'
-      width={200}
-      height={200}/>
+
       <div className='flex flex-col items-center mt-8'>
         <h2 className='font-bold text-[40px] py-3'>What should we call your team?</h2>
         <h2 className='text-gray-500'>You can always change this later from settings.</h2>
